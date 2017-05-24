46ce8f93cb878bcbe269a50335f3bc100f27c4a3
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-05-24 16:21:08 -0700

    Port documentation to repo

---
+++ scripts/generate_api.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Used to generate the API HTML
+
+set -e -o pipefail
+
+REPO_DIR="$(git rev-parse --show-toplevel)"
+API_VERSION="${1:-""}"
+RAML_DIR="${2:-"$REPO_DIR/docs"}"
+TARGET_DIR="${3:-"$REPO_DIR/target/api-$API_VERSION"}"
+
+mkdir -p "$TARGET_DIR"
+
+# Build a docker image with raml2html
+sudo docker build --tag ramltools "$REPO_DIR/docker/ramltools"
+
+for RAML in $RAML_DIR/*.raml
+do
+  REPO_DIR_DOCKER="/src"
+  RAML_DOCKER="$REPO_DIR_DOCKER/${RAML##"$REPO_DIR/"}"
+
+  BASENAME="$(basename "$RAML")"
+  NAME="${BASENAME%.*}"
+  OUT_RAML="$TARGET_DIR/$NAME.raml"
+  OUT_HTML="$TARGET_DIR/$NAME.html"
+  OUT_SWAGGER="$TARGET_DIR/$NAME.swagger"
+
+  # Generate the API HTML using the built docker image
+  echo "Generating $OUT_HTML"
+  sudo docker run \
+    --volume="$REPO_DIR":"$REPO_DIR_DOCKER":ro \
+    --rm ramltools /usr/local/bin/raml2html \
+    "$RAML_DOCKER" > "$OUT_HTML"
+
+  # Generate the API Swagger using the built docker image
+  echo "Generating $OUT_SWAGGER"
+  sudo docker run \
+    --volume="$REPO_DIR":"$REPO_DIR_DOCKER":ro \
+    --rm ramltools /usr/local/bin/raml2swagger \
+    "$RAML_DOCKER" > "$OUT_SWAGGER"
+
+  # Copy the API RAML
+  echo "Generating $OUT_RAML"
+  cp "$RAML" "$OUT_RAML"
+
+done
